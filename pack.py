ae33365a4e9f9fa6a9fcacffeb6c4a189c683e04
Evan Young <evanyoung62@icloud.com>
2017-03-11 14:13:05 -0600

    Cross-Platform Automation
    And support email

--- pack.py
+++ pack.py
@@ -4,11 +4,15 @@
 ##
 import bz2
 import os
+import platform
 
 path = os.path.dirname(os.path.realpath(__file__))
 
-writer = bz2.BZ2File('Packages.bz2', 'w') 
-pkg = open("{0}\Packages".format(path), 'rb')
+writer = bz2.BZ2File("Packages.bz2", 'w')
+if (platform.system() == 'Darwin'):
+    pkg = open("{0}/Packages".format(path), 'rb')
+else:
+    pkg = open("{0}\Packages".format(path), 'rb')
 
 for l in pkg:
     writer.write(l)
