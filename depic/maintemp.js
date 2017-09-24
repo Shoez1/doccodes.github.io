13ff98da36dd6a9714666ade65e8f387bd81f820
DocCodes <evanyoung62@icloud.com>
2017-09-24 17:57:33 -0500

    Code Tidying
    Rewrote indentation and quotes in most files

--- depic/maintemp.js
+++ depic/maintemp.js
@@ -7,12 +7,12 @@ var date = document.getElementById('update').innerHTML;
 var pkgs = 16;
 
 function getFooter() {
-	var ctt = new Date();
-	var dct = new Date(ctt.getTime()+(ctt.getTimezoneOffset()*60*1000)-(5*60*60*1000));
-	hr = dct.getHours()%12;
-	hr==0?hr=12:hr=hr;
-	timeSt = `${hr}:${dct.getMinutes()<10?'0'+dct.getMinutes():dct.getMinutes()}:${dct.getSeconds()<10?'0'+dct.getSeconds():dct.getSeconds()} ${dct.getHours()>=12?'PM':'AM'}`;	
-	window.footSt = `Hosting ${pkgs} Packages<br>Currently: ${timeSt}<br>Copyright Doc ${ctt.getUTCFullYear()}`;
+   var ctt = new Date();
+   var dct = new Date(ctt.getTime()+(ctt.getTimezoneOffset()*60*1000)-(5*60*60*1000));
+   hr = dct.getHours()%12;
+   hr==0?hr=12:hr=hr;
+   timeSt = `${hr}:${dct.getMinutes()<10?'0'+dct.getMinutes():dct.getMinutes()}:${dct.getSeconds()<10?'0'+dct.getSeconds():dct.getSeconds()} ${dct.getHours()>=12?'PM':'AM'}`;   
+   window.footSt = `Hosting ${pkgs} Packages<br>Currently: ${timeSt}<br>Copyright Doc ${ctt.getUTCFullYear()}`;
 }
 getFooter();
 setInterval(getFooter, 1000);
@@ -20,85 +20,92 @@ setInterval(getFooter, 1000);
 var template = ' \
 <!DOCTYPE html> \
 <html> \
-	<head> \
-		<meta name="viewport" content="width=device-width, initial-scale=1"> \
-		<meta http-equiv="content-type" content="text/html" charset="utf-8"/> \
-		<link rel="stylesheet" type="text/css" href="../styles.css"> \
-		<meta charset="utf-8"> \
-		<title>Doc\'s Repo</title> \
-	</head> \
-	<body> \
-		<header> \
-			<h1>'+title+'</h1> \
-		</header> \
-		<div id="content"> \
-			<h2 role="header">Description</h2> \
-			<ul> \
-				<li> \
-					<p><strong>Compatible with iOS '+compat+'</strong></p> \
-					'+desc+' \
-				</li> \
-				<li> \
-					<a href="screenshots.html" role="button" class="cydia_blank">View Screenshots</a> \
-				</li> \
-			</ul> \
-			<h2 role="header">Changelog</h2> \
-			<ul> \
-				<li> \
-					<p><strong>Changes in Version '+ver+'</strong> &middot; '+date+'<br></p><p></p> \
-					<ul>'+change+'</ul> \
-				</li> \
-				<li> \
-					<a href="changelog.html" role="button" class="cydia_blank">Full Changelog</a> \
-				</li> \
-			</ul> \
-			<h2 role="header">Social Media</h2> \
-			<ul> \
-                <li> \
-                    <a href="https://paypal.me/DocCodes" role="button">Donate to further development</a> \
-                </li> \
-				<li> \
-					<a href="https://www.twitter.com/Bowser65" role="button">Follow on Twitter</a> \
-				</li> \
-				<li> \
-					<a href="https://www.github.com/DocCodes" role="button">View on GitHub</a> \
-				</li> \
-				<li> \
-					<a href="https://www.reddit.com/user/British-Mystery" role="button">View on Reddit</a> \
-				</li> \
-			</ul> \
-		</div> \
-		<footer role="footer">'+footSt+'</footer> \
-	</body> \
+   <head> \
+      <title>Doc\'s Repo</title> \
+      <meta name="author" content="Evan &quot;Doc&quot; Young"> \
+      <meta name="description" content="Where I host my Cydia tweaks"> \
+      <meta name="keywords" content="jailbreak, doc, code, cydia"> \
+      <meta name="copyright" content="&copy; 2017 Evan Young"> \
+      <meta charset="utf-8"> \
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"> \
+      <meta name="robots" content="index, nofollow"> \
+      <meta name="html-valid" content="HTML5, ARIA, SVG1.1, MathML 2.0"> \
+      <meta name="css-valid" content="CSSL 3"> \
+      <meta name="lighthouse" content="281; A+"> \
+      <link rel="stylesheet" type="text/css" href="../styles.css"> \
+   </head> \
+   <body> \
+      <header> \
+         <h1>'+title+'</h1> \
+      </header> \
+      <div id="content"> \
+         <h2 role="header">Description</h2> \
+         <ul> \
+            <li> \
+               <p><strong>Compatible with iOS '+compat+'</strong></p> \
+               '+desc+' \
+            </li> \
+            <li> \
+               <a href="screenshots.html" role="button" class="cydia_blank">View Screenshots</a> \
+            </li> \
+         </ul> \
+         <h2 role="header">Changelog</h2> \
+         <ul> \
+            <li> \
+               <p><strong>Changes in Version '+ver+'</strong> &middot; '+date+'<br></p><p></p> \
+               <ul>'+change+'</ul> \
+            </li> \
+            <li> \
+               <a href="changelog.html" role="button" class="cydia_blank">Full Changelog</a> \
+            </li> \
+         </ul> \
+         <h2 role="header">Social Media</h2> \
+         <ul> \
+               <li> \
+                  <a href="https://paypal.me/DocCodes" role="button">Donate to further development</a> \
+               </li> \
+            <li> \
+               <a href="https://www.twitter.com/Bowser65" role="button">Follow on Twitter</a> \
+            </li> \
+            <li> \
+               <a href="https://www.github.com/DocCodes" role="button">View on GitHub</a> \
+            </li> \
+            <li> \
+               <a href="https://www.reddit.com/user/British-Mystery" role="button">View on Reddit</a> \
+            </li> \
+         </ul> \
+      </div> \
+      <footer role="footer">'+footSt+'</footer> \
+   </body> \
 </html> \
 ';
 var docElement = document.documentElement;
 docElement.innerHTML = template;
 
 function checkCydia() {
-	var classList = docElement.classList;
-	if (navigator.userAgent.indexOf("Cydia") != -1) {
-		if (document.title.indexOf(" \u00b7 ") != -1) {
-			document.title = document.title.split(" \u00b7 ")[0];
-		}
-		classList.add("cydia");
-	} else {
-		classList.remove("cydia", "depiction");
-	}
+   var classList = docElement.classList;
+   if (navigator.userAgent.indexOf("Cydia") != -1) {
+      if (document.title.indexOf(" \u00b7 ") != -1) {
+         document.title = document.title.split(" \u00b7 ")[0];
+      }
+      classList.add("cydia");
+   } else {
+      classList.remove("cydia", "depiction");
+   }
 }
 function correctCydia() {
-	if (document.documentElement.classList.contains("cydia")) {
-		var base = document.createElement("base");
-		var cydiaBlankLinks = document.getElementsByClassName("cydia_blank");
-		base.target = "_open";
-		document.head.appendChild(base);
-		for(var i = 0; i < cydiaBlankLinks.length; i++) {
-			cydiaBlankLinks[i].target = "_blank";
-		}
-		
-		document.querySelector("header").remove();
-		document.querySelector("body").style.margin = "0 0 35px 0";
-	}
+   if (document.documentElement.classList.contains("cydia")) {
+      var base = document.createElement("base");
+      var cydiaBlankLinks = document.getElementsByClassName("cydia_blank");
+      base.target = "_open";
+      document.head.appendChild(base);
+      for(var i = 0; i < cydiaBlankLinks.length; i++) {
+         cydiaBlankLinks[i].target = "_blank";
+      }
+      
+      document.querySelector("header").remove();
+      document.querySelector("body").style.margin = "0 0 35px 0";
+   }
 }
 checkCydia();
 correctCydia();
--- depic/subtemp.js
+++ depic/subtemp.js
@@ -3,12 +3,12 @@ var main = document.getElementById('main').innerHTML;
 var pkgs = 16;
 
 function getFooter() {
-	var ctt = new Date();
-	var dct = new Date(ctt.getTime()+(ctt.getTimezoneOffset()*60*1000)-(5*60*60*1000));
-	hr = dct.getHours()%12;
-	hr==0?hr=12:hr=hr;
-	timeSt = `${hr}:${dct.getMinutes()<10?'0'+dct.getMinutes():dct.getMinutes()}:${dct.getSeconds()<10?'0'+dct.getSeconds():dct.getSeconds()} ${dct.getHours()>=12?'PM':'AM'}`;	
-	window.footSt = `Hosting ${pkgs} Packages<br>Currently: ${timeSt}<br>Copyright Doc ${ctt.getUTCFullYear()}`;
+   var ctt = new Date();
+   var dct = new Date(ctt.getTime()+(ctt.getTimezoneOffset()*60*1000)-(5*60*60*1000));
+   hr = dct.getHours()%12;
+   hr==0?hr=12:hr=hr;
+   timeSt = `${hr}:${dct.getMinutes()<10?'0'+dct.getMinutes():dct.getMinutes()}:${dct.getSeconds()<10?'0'+dct.getSeconds():dct.getSeconds()} ${dct.getHours()>=12?'PM':'AM'}`;   
+   window.footSt = `Hosting ${pkgs} Packages<br>Currently: ${timeSt}<br>Copyright Doc ${ctt.getUTCFullYear()}`;
 }
 getFooter();
 setInterval(getFooter, 1000);
@@ -16,48 +16,55 @@ setInterval(getFooter, 1000);
 var template = ' \
 <!DOCTYPE html> \
 <html> \
-	<head> \
-		<meta name="viewport" content="width=device-width, initial-scale=1"> \
-		<meta http-equiv="content-type" content="text/html" charset="utf-8"/> \
-		<link rel="stylesheet" type="text/css" href="../styles.css"> \
-		<meta charset="utf-8"> \
-		<title>Doc\'s Repo</title> \
-	</head> \
-	<body style="margin: 80px 0px 35px 0px;"> \
-		<header> \
-			<h1>'+title+'</h1> \
-		</header> \
-		<div id="content"> \
-            '+main+' \
-		</div> \
-		<footer role="footer">'+footSt+'</footer> \
-	</body> \
+   <head> \
+      <title>Doc\'s Repo</title> \
+      <meta name="author" content="Evan &quot;Doc&quot; Young"> \
+      <meta name="description" content="Where I host my Cydia tweaks"> \
+      <meta name="keywords" content="jailbreak, doc, code, cydia"> \
+      <meta name="copyright" content="&copy; 2017 Evan Young"> \
+      <meta charset="utf-8"> \
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"> \
+      <meta name="robots" content="index, nofollow"> \
+      <meta name="html-valid" content="HTML5, ARIA, SVG1.1, MathML 2.0"> \
+      <meta name="css-valid" content="CSSL 3"> \
+      <meta name="lighthouse" content="281; A+"> \
+      <link rel="stylesheet" type="text/css" href="../styles.css"> \
+   </head> \
+   <body style="margin: 80px 0px 35px 0px;"> \
+      <header> \
+         <h1>'+title+'</h1> \
+      </header> \
+      <div id="content"> \
+         '+main+' \
+      </div> \
+      <footer role="footer">'+footSt+'</footer> \
+   </body> \
 </html> \
 ';
 var docElement = document.documentElement;
 docElement.innerHTML = template;
 
 function checkCydia() {
-	var classList = docElement.classList;
-	if (navigator.userAgent.indexOf("Cydia") != -1) {
-		if (document.title.indexOf(" \u00b7 ") != -1) {
-			document.title = document.title.split(" \u00b7 ")[0];
-		}
-		classList.add("cydia");
-	} else {
-		classList.remove("cydia", "depiction");
-	}
+   var classList = docElement.classList;
+   if (navigator.userAgent.indexOf("Cydia") != -1) {
+      if (document.title.indexOf(" \u00b7 ") != -1) {
+         document.title = document.title.split(" \u00b7 ")[0];
+      }
+      classList.add("cydia");
+   } else {
+      classList.remove("cydia", "depiction");
+   }
 }
 function correctCydia() {
-	if (document.documentElement.classList.contains("cydia")) {
-		var base = document.createElement("base");
-		var cydiaBlankLinks = document.getElementsByClassName("cydia_blank");
-		base.target = "_open";
-		document.head.appendChild(base);
-		for(var i = 0; i < cydiaBlankLinks.length; i++) {
-			cydiaBlankLinks[i].target = "_blank";
-		}
-	}
+   if (document.documentElement.classList.contains("cydia")) {
+      var base = document.createElement("base");
+      var cydiaBlankLinks = document.getElementsByClassName("cydia_blank");
+      base.target = "_open";
+      document.head.appendChild(base);
+      for(var i = 0; i < cydiaBlankLinks.length; i++) {
+         cydiaBlankLinks[i].target = "_blank";
+      }
+   }
 }
 checkCydia();
 correctCydia();
